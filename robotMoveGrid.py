c21c1d7a4d3fa5e19ce7015e83ef64541399afde
jianbo888 <69053996+jianbo888@users.noreply.github.com>
2020-08-19 14:50:01 -0400

    Create robotMoveGrid.py

---
+++ robotMoveGrid.py
@@ -0,0 +1,13 @@
+"""
+
+A robot sits on the upper left corner of a r x c grid, it can only move to right or down. 
+There are cells which is off grid (False), find a path for the robot to go to the bottom right of the grid
+
+"""
+
+
+"""
+Assume grid[r][c] is a boolean grid, if the value is false the cell is off grid.
+"""
+
+            
